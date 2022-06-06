89d3460c0017eaf219c41d84313f011e894c1234
AconCavy <aconcavy@gmail.com>
2022-06-06 11:36:15 +0900

    Init Statiq

--- src/Portfolio/Program.cs
+++ src/Portfolio/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Statiq.App;
+using Statiq.Web;
+
+
+await Bootstrapper.Factory
+    .CreateWeb(args)
+    .RunAsync();
